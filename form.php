9e381536c3e044336263ac0f91de9a9cc5e706cb
solidmaradini <solidmaradini@gmail.com>
2021-11-16 10:34:45 +0100

    fixe HTML & CSS

---
+++ form.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    
+    <title>Document</title>
+</head>
+<body>
+    
+
+   
+
+    <div class="input-group mb-3">
+    <input type="text" class="form-control" placeholder="Name" aria-label="Name">
+    
+    </div>
+
+    <div class="input-group mb-3">
+    <input type="text" class="form-control" placeholder="Consult subject" aria-label="Consult subject">
+     </div>
+     <button type="button" class="btn btn-primary">SUBMIT</button>
+
+  
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
