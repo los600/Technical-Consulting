fbcec3f2d7e6f6bae35ca9bd41cd11a6ee17db16
solidmaradini <solidmaradini@gmail.com>
2021-11-17 09:06:17 +0100

    inclued PHP in HTML

--- main.php
+++ main.php
@@ -8,10 +8,15 @@
     <title>Document</title>
 </head>
 <body>
-    <div class="alert alert-primary" role="alert">
-    A simple primary alert—check it out!
-    </div>
-  
+    <section class='justify-content-center'>
+        <?php echo "
+            <div class='alert alert-primary' role='alert'>
+            A simple primary alert—check it out!
+            </div><div class='alert alert-primary' role='alert'>
+            A simple primary alert—check it out!
+            </div>
+        "?>
+    </section>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
