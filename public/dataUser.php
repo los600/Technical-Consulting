e36ac55d3830892ac7ca87af16f727936c025f84
Rubén <rubencaporte@gmail.com>
2021-11-17 12:49:40 +0100

    database

--- public/dataUser.php
+++ public/dataUser.php
@@ -1,21 +1,31 @@
-
 <?php
-
+require_once 'DataBase.php';
 class DataUser {
-    public $id;
+    
+    private $id;
     public $username;
     public $subject;
-    private $creationTime;
+    public $creationTime;
+    private $db;
+
 
     public function __Construct ($data){
         $this->id = $data['id'];
         $this->username= $data['username'];
         $this->subject = $data ['subject'];
         $this->creationTime = $data['creationTime'];
+        $this->db =new Database
       
 
     }
+    public function all() {
+        $userList =[];
+        foreach ($this->db->getAll()as $users){
+            array_push($userList,$users);
+        }
 
+    }
+    return $userList
 
 }
 
