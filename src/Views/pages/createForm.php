f6cec2689adb01402a9b383d557f96ceec75cce6
solidmaradini <solidmaradini@gmail.com>
2021-11-23 14:35:58 +0100

    fixed form

--- src/Views/pages/createForm.php
+++ src/Views/pages/createForm.php
@@ -1,13 +1,13 @@
 <?php require_once __DIR__ . '/../layouts/default.php'; ?>
 
     <main>
-            <form action="/" name="form" method="post" id=user>
-            <div class="input-group m-3">
-                <input type="text" class="form-control" id="user"value="" name="username" placeholder="Name" aria-label="Name">
-            </div>
-            <div class="input-group m-3">
-                <input type="text" class="form-control"  id="user" name="subject" placeholder="Consult subject " aria-label="Consult subject">
-            </div>
-            <button type="button" class="btn btn-primary m-3" name="connectdata">SUBMIT</button>
+        <form action="/" name="form" method="post" id=user>
+                <div class="input-group m-3">
+                    <input type="text" class="form-control" id="user"value="" name="username" placeholder="Name" aria-label="Name">
+                </div>
+                <div class="input-group m-3">
+                    <input type="text" class="form-control"  id="user" name="subject" placeholder="Consult subject " aria-label="Consult subject">
+                </div>
+                <button type="submit" class="btn btn-primary m-3" >SUBMIT</button>
         </form>
     </main>
