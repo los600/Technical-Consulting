4e9ab15ae5b33762f2565f49965632c0254160db
marteta91 <martaguitart938@gmail.com>
2021-11-29 11:38:46 +0100

    sistem

--- src/Core/SQLConexion.php
+++ src/Core/SQLConexion.php
@@ -10,10 +10,11 @@ class SQLConexion{
 
     public function __construct(){
 
-        $host = "localhost";
-        $user = "root";
-        $pass = "";
-        $database = "technicalconsulting";
+        $config =new Config();
+        $host = $config->host;
+        $user = $config->user;
+        $pass = $config->pass;
+        $database = $config->database;
         $charset = "utf-8";
         $options = [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC];
         $this->mysql = new PDO ("mysql:host={$host};dbname={$database}",$user,$pass,$options);
