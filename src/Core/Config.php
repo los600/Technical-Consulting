49d86a6887d1984358302c386cbc8c96da92c9cc
marteta91 <martaguitart938@gmail.com>
2021-11-29 11:48:10 +0100

    ignore

---
+++ src/Core/Config.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Core;
+
+class config {
+    public $host ="localhost";
+    public $user = "root"; 
+    public $pass = ""; 
+    public $database = "Tecnical-consulting";
+}
